0b9eceb4fe25c2e6e68aef766c6c0aa75afcb1cc
hermione-see <20019634@myrp.edu.sg>
2021-06-04 17:16:35 +0800

    Add and Clear

--- app/src/main/java/sg/edu/rp/c346/id20019634/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019634/simpletodo/MainActivity.java
@@ -3,12 +3,57 @@ package sg.edu.rp.c346.id20019634.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTask;
+    Button btnAdd, btnClear;
+    ListView lvTasks;
+    ArrayList<String> alTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTask = findViewById(R.id.editTextTask);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnClear = findViewById(R.id.buttonClear);
+        lvTasks = findViewById(R.id.listViewTasks);
+
+        alTasks = new ArrayList<String>();
+
+        ArrayAdapter aaTasks = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTasks);
+        lvTasks.setAdapter(aaTasks);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newTask = etTask.getText().toString();
+                alTasks.add(newTask);
+                aaTasks.notifyDataSetChanged();
+                etTask.setText(null);
+
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alTasks.clear();
+                aaTasks.notifyDataSetChanged();
+
+            }
+        });
+
     }
 }
